d6801f6e202d77874d5bfd565689b989895886bb
Mayhendraadithia <mayhendraadithia4@gmail.com>
2024-11-12 16:55:53 +0700

    no result

--- resources/views/UserAdmin/layout.blade.php
+++ resources/views/UserAdmin/layout.blade.php
@@ -67,7 +67,7 @@
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
-                    <i class="fas fa-fw fa-cog"></i>
+                    <i class="bi bi-book-half"></i>
                     <span>Materi</span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
